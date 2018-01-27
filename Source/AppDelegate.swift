9cf64d0e880f08c0b971b54bd3f75c49e91c14b0
dondi <dondi@lmu.edu>
2018-01-27 14:53:04 -0800

    Wire up service.

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -5,15 +5,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NSApp.servicesProvider = self
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func run(_ pboard: NSPasteboard, userData: String, error: NSErrorPointer) {
+        if let str = pboard.string(forType: NSPasteboard.PasteboardType.string) {
+            print("Submitted to service: \(str)")
+        }
+    }
 }
-
