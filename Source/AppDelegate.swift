e5ef72501a96dfd7200044b4106dbf8401a5c8dc
dondi <dondi@lmu.edu>
2018-01-28 12:47:33 -0800

    First cut at execution, hardcoded to Python.

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -14,8 +14,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @objc func run(_ pboard: NSPasteboard, userData: String, error: NSErrorPointer) {
-        if let str = pboard.string(forType: NSPasteboard.PasteboardType.string) {
-            print("Submitted to service: \(str)")
+        guard let str = pboard.string(forType: NSPasteboard.PasteboardType.string) else {
+            // TODO End silently or complain about the wrong data before returning?
+            return
+        }
+
+        // Thank you https://iswift.org/cookbook/execute-a-shell-command
+        let process = Process()
+        process.launchPath = "/usr/bin/python"
+        process.arguments = ["-c", str]
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+
+        process.launch()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        if let output = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+            print("Runner output:\n\(output)")
+        } else {
+            print("Runner: No output")
         }
     }
 }
