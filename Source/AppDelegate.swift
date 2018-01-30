ef292ef8551617a30be2706c587c3fb4907e40b0
dondi <dondi@lmu.edu>
2018-01-30 10:21:45 -0800

    Shift project to a document-based app.

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -3,39 +3,16 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-    @IBOutlet var console: NSTextView!
+
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        NSApp.servicesProvider = self
+        // Insert code here to initialize your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-    @objc func run(_ pboard: NSPasteboard, userData: String, error: NSErrorPointer) {
-        guard let str = pboard.string(forType: NSPasteboard.PasteboardType.string) else {
-            // TODO End silently or complain about the wrong data before returning?
-            return
-        }
 
-        // Thank you https://iswift.org/cookbook/execute-a-shell-command
-        let process = Process()
-        process.launchPath = "/usr/bin/python"
-        process.arguments = ["-c", str]
-
-        let pipe = Pipe()
-        process.standardOutput = pipe
-
-        process.launch()
-
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        var result = "(executed, but no output)"
-        if let output = String(data: data, encoding: String.Encoding.utf8) {
-            result = output
-        }
-
-        console.string = "\(console.string)\n-----\n\nCode:\n\n\(str)\n\n-----\n\n\(result)"
-    }
 }
+
