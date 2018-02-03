5fbb0ad089cc61f5ff707238ebfc8ecf99c4ef3f
dondi <dondi@lmu.edu>
2018-02-03 14:23:06 -0800

    Restore advertised Run This service.

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -3,16 +3,21 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NSApp.servicesProvider = self
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func run(_ pboard: NSPasteboard, userData: String, error: NSErrorPointer) {
+        if let str = pboard.string(forType: NSPasteboard.PasteboardType.string) {
+            if let newNSDocument = try? NSDocumentController.shared.openUntitledDocumentAndDisplay(true),
+               let newDocument = newNSDocument as? Document {
+                newDocument.codeTextView.string = str
+                newDocument.runThis(self)
+            }
+        }
+    }
 }
-
